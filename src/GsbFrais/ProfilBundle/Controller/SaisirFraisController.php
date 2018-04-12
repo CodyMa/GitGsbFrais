1a7555feba80c67c44dc98444ccae0e15e64b549
CodyMa <cody.macedo.sio@gmail.com>
2018-04-12 16:08:56 +0200

    0.5 Cody

--- src/GsbFrais/ProfilBundle/Controller/SaisirFraisController.php
+++ src/GsbFrais/ProfilBundle/Controller/SaisirFraisController.php
@@ -2,6 +2,7 @@
 
 namespace GsbFrais\ProfilBundle\Controller;
 
+use Doctrine\DBAL\Types\TextType;
 use GsbFrais\ProfilBundle\Entity\FicheFrais;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
@@ -14,20 +15,31 @@ class SaisirFraisController extends Controller
         $session = $request->getSession();
         //echo $session->get('id');
         $ficheFrais = new FicheFrais();
-        /*
+
         $form = $this->createFormBuilder($ficheFrais)
-            ->add('login', TextType::class, array('label'=> 'Identifiant' ,'attr' => array('class'=> 'form-control')))
-            ->add('mdp', PasswordType::class, array('label'=> 'Mot de passe' ,'attr' => array('class'=> 'form-control')))
-            ->add('choices', ChoiceType::class, array('label'=> 'Statut' , 'attr' => array('class'=> 'custom-select'),
+            ->add('PeriodeEngagement', TextType::class, array('label'=> 'Identifiant' ,'attr' => array('class'=> 'form-control')))
+            ->add('RepasMidi', TextType::class, array('label'=> 'Mot de passe' ,'attr' => array('class'=> 'form-control')))
+            ->add('Nuitees', TextType::class, array('label'=> 'Mot de passe' ,'attr' => array('class'=> 'form-control')))
+            ->add('Etape', TextType::class, array('label'=> 'Identifiant' ,'attr' => array('class'=> 'form-control')))
+            ->add('km', TextType::class, array('label'=> 'Identifiant' ,'attr' => array('class'=> 'form-control')))
+            ->add('Date', TextType::class, array('label'=> 'Identifiant' ,'attr' => array('class'=> 'form-control')))
+            ->add('Libelle', TextType::class, array('label'=> 'Identifiant' ,'attr' => array('class'=> 'form-control')))
+            ->add('', TextType::class, array('label'=> 'Identifiant' ,'attr' => array('class'=> 'form-control')))
+            ->add('nombreJustificatif', TextType::class, array('label'=> 'Identifiant' ,'attr' => array('class'=> 'form-control')))
+            ->add('MontantTotal', TextType::class, array('label'=> 'Identifiant' ,'attr' => array('class'=> 'form-control')))
+            ->add('', ChoiceType::class, array('label'=> 'Statut' , 'attr' => array('class'=> 'custom-select'),
                 'choices'  => array(
                     'Statut :' => array('Visiteur' => 'visiteur', 'Comptable' => 'comptable', ),
                 )))
             ->add('save', SubmitType::class, array('label'=> 'Se connecter' ,'attr' => array('class'=> 'btn btn-primary',  'id' => 'btnSave')))
             ->getForm();
-        */
+
 
         return $this->render(
-            'profil/saisieFrais.html.twig'
+            'profil/saisieFrais.html.twig' ,
+            array(
+                'form' => $form->createView()
+            )
         );
     }
 }
