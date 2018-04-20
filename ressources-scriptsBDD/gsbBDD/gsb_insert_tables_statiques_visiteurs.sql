aa5dcf21fa48ecc42eccad6f6db7f983eb0dde59
nico <nicolas.bourneuf.sio@gmail.com>
2018-04-20 16:26:54 +0200

    fini 20/04_17h

--- ressources-scriptsBDD/gsbBDD/gsb_insert_tables_statiques_visiteurs.sql
+++ ressources-scriptsBDD/gsbBDD/gsb_insert_tables_statiques_visiteurs.sql
@@ -3,11 +3,11 @@ use gsbFraisPdo
 -- Contenu de la table `FraisForfait`
 --
 
-INSERT INTO `FraisForfait` (`id`, `libelle`, `montant`) VALUES
-('ETP', 'Forfait Etape', 110.00),
-('KM', 'Frais Kilométrique', 0.62),
-('NUI', 'Nuitée Hôtel', 80.00),
-('REP', 'Repas Restaurant', 25.00);
+INSERT INTO `frais_forfait` (`libelle`, `montant`) VALUES
+('Forfait Etape (nuit et repas)', 110.00),
+('Frais Kilométrique', 0.62),
+('Nuitée Hôtel', 80.00),
+('Repas Restaurant', 25.00);
 --
 -- Contenu de la table `LigneFraisForfait`
 --
--- src/GsbFrais/AccueilBundle/Controller/AccueilController.php
+++ src/GsbFrais/AccueilBundle/Controller/AccueilController.php
@@ -8,8 +8,12 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
 class AccueilController extends Controller
 {
-    public function indexAction()
+    public function indexAction(Request $request)
     {
+        $session = $request->getSession();
+        // ici ce pour effacer toutes les sessions
+        $session->clear();
+
         return $this->render('accueil/accueil.html.twig');
     }
 
